a7f39c60d7050553334b04c44b0336d0cd758cb1
Andres <andresgomewe@gmail.com>
2022-12-11 19:40:18 -0500

    todoReducer agregado

--- UseTodos/UseTodos.js
+++ UseTodos/UseTodos.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useReducer } from "react";
-import { todoReducer } from "../08-useReducer/todoreducer";
+import { todoReducer } from "./todoreducer";
 
 
 const initialState = [];
